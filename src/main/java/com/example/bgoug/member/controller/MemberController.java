02c5fd9dcf5e0cbde3720a800a48bc975460e6b4
Veselin <v.sariev@abv.bg>
2019-08-20 12:11:21 +0300

    new version

--- src/main/java/com/example/bgoug/member/controller/MemberController.java
+++ src/main/java/com/example/bgoug/member/controller/MemberController.java
@@ -9,6 +9,7 @@ import com.example.bgoug.company.services.CompanyService;
 import com.example.bgoug.errors.Error;
 import com.example.bgoug.events.entities.Event;
 import com.example.bgoug.events.models.ViewModels.EventView;
+import com.example.bgoug.events.models.bindingModels.EditEventModel;
 import com.example.bgoug.events.services.EventService;
 import com.example.bgoug.member.entities.Member;
 import com.example.bgoug.member.enums.MemberType;
@@ -34,7 +35,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("member")
+@RequestMapping("members")
 public class MemberController {
 
     @Autowired
@@ -71,7 +72,7 @@ public class MemberController {
             memberViews.add(memberView);
         }
         model.addAttribute("members", memberViews);
-        model.addAttribute("view", "/member/irregular-members");
+        model.addAttribute("view", "/members/irregular-members");
         return "base-layout";
     }
 
@@ -79,7 +80,7 @@ public class MemberController {
     public String getAllMembersPage(Model model) {
         List<MemberView> memberViews = this.memberService.getAll();
         model.addAttribute("members", memberViews);
-        model.addAttribute("view", "member/member-table");
+        model.addAttribute("view", "members/member-table");
         return "base-layout";
     }
 
@@ -90,90 +91,71 @@ public class MemberController {
 //        Long memberId = memberView.getId();
         List<MemberView> memberViews = this.memberService.findAllByCompany(member.getCompany());
         model.addAttribute("members", memberViews);
-        model.addAttribute("view", "member/members-by-company");
+        model.addAttribute("view", "members/members-by-company");
         return "base-layout";
     }
+    
+    @GetMapping("/edit/{id}")
+    public String getEditPage(Model model, @PathVariable Long id) {
+        EditMemberModel editMemberModel = this.memberService.findMemberById(id);
+        List<CompanyView> companies = this.companyService.getAll();
+        List<ApplicationView> applicationViews = this.applicationService.findAllApplications();
+        
+        model.addAttribute("view", "/members/member-edit");
+        model.addAttribute("type", "Edit");
+        model.addAttribute("member", editMemberModel);
+        model.addAttribute("companies", companies);
+        model.addAttribute("applicationsList", applicationViews);
+        return "base-layout";
+    }
+    
+    @PostMapping("/edit/{id}")
+    public String postEditPage(@ModelAttribute EditMemberModel editMemberModel,
+    		                                   @PathVariable Long id,
+    		                                   @RequestParam String pcPlatform,
+    		                                   @RequestParam String isPayed,
+    		                                   @RequestParam String companyName,
+    		                                   @RequestParam List<String> applicationsList) {
+                                    
 
-//    @GetMapping("add")
-//    public String getAddMemberPage(Model model) {
-//        List<CompanyView> companies = this.companyService.getAll();
-//        List<EventView> eventViews = this.eventService.getAllEvents();
-//        List<MemberView> memberViews = this.memberService.getAll();
-//        List<ApplicationView> applicationViews = this.applicationService.findAllApplications();
-//        model.addAttribute("applicationsList", applicationViews);
-//        model.addAttribute("membersViews", memberViews);
-//        model.addAttribute("events", eventViews);
-//        model.addAttribute("companies", companies);
-//        model.addAttribute("members", new MemberModel());
-//        model.addAttribute("view", "member/member-add");
-//        return "base-layout";
-//    }
-//
-//    @PostMapping("add")
-//    public String addMemberPage(@Valid @ModelAttribute MemberModel memberModel,
-//                                @RequestParam String companyName,
-//                                @RequestParam String memberType,
-//                                @RequestParam Boolean isPayed,
-//                                @RequestParam String event,
-//                                @RequestParam String recommendedName,
-//                                @RequestParam String pcPlatform,
-//                                @RequestParam List<String> applicationsList) {
-//
-//        Company companyModel = this.companyService.getCompanyByName(companyName);
-//        memberModel.setCompany(companyModel);
-//
-//        if (!(event.equals(""))){
-//            Event eventToPersist = this.eventService.getEventByName(event);
-//            memberModel.getEvents().add(eventToPersist);
-//        }
-//
-//        memberModel.setMemberType(memberType);
-//        memberModel.setMembershipFee(isPayed);
-//
-//        String value = PcPlatform.valueOf(pcPlatform).getKey();
-//        memberModel.setPcPlatform(value);
-//
-//        if (!(recommendedName.equals(""))) {
-//
-//            // find recommended member from database
-//            EditMemberModel recommendedMember = this.memberService.findMemberByName(recommendedName);
-//
-//            ModelMapper modelMapper = new ModelMapper();
-//
-//            //convert recommended member to member
-//            Member recommended = modelMapper.map(recommendedMember, Member.class);
-//
-//            // get all recommended Names from recommended member
-//            Set<String> recommendedMemberNames = recommended.getRecommendedMembers();
-//
-//            // add recommended member to currentModel
-//            memberModel.getRecommendedMembers().add(recommendedName);
-//
-//            // iterate over recommended names
-//            if (recommendedMemberNames.size() > 0) {
-//                for (String currentName : recommendedMemberNames) {
-//
-//                    EditMemberModel editMemberModel = this.memberService.findMemberByName(currentName);
-//                    this.memberService.update(editMemberModel);
-//                    memberModel.getRecommendedMembers().add(currentName);
-//                }
-//                this.memberService.update(recommendedMember);
-//            } else {
-//
-//                this.memberService.update(recommendedMember);
-//            }
-//        }
-//
-//        for (String currentApplication : applicationsList) {
-//            Application application = this.applicationService.findApplicationByName(currentApplication);
-//            memberModel.getApplications().add(application);
-//        }
-//
-//        this.memberService.persist(memberModel);
-//
-//        return "redirect:/member/all";
-//
-//    }
+        editMemberModel.setId(id);
+        editMemberModel.setPcPlatform(pcPlatform);
+        Company currentCompany = this.companyService.getCompanyByName(companyName);
+        editMemberModel.setCompany(currentCompany);
+        
+        for (String applicationName : applicationsList) {
+			Application application = this.applicationService.findApplicationByName(applicationName);
+			editMemberModel.getApplications().add(application);
+		}
+        
+        if (isPayed.equals("yes")) {
+			editMemberModel.setMembershipFee(true);
+		} else {
+			editMemberModel.setMembershipFee(false);
+		}
+        this.memberService.update(editMemberModel);
+        return "redirect:/members/all";
+    }
+    
+    @GetMapping("/delete/{id}")
+    public String getDeletePage(Model model, @PathVariable Long id){
+        EditMemberModel editMemberModel = this.memberService.findMemberById(id);
+        Company company = this.companyService.getCompanyByName(editMemberModel.getCompany().getName());
+        model.addAttribute("company", company.getName());
+        model.addAttribute("view", "/members/member-delete");
+        model.addAttribute("type", "Delete");
+        model.addAttribute("member", editMemberModel);
+        return "base-layout";
+    }
+    
+    
+
+    @PostMapping("/delete/{id}")
+    public String postDeletePage(@ModelAttribute EditMemberModel editMemberModel, @PathVariable Long id){
+        editMemberModel.setId(id);
+        this.memberService.delete(editMemberModel);
+        return "redirect:/members/all";
+    }
 
 
     @GetMapping("register")
@@ -186,8 +168,9 @@ public class MemberController {
         model.addAttribute("membersViews", memberViews);
         model.addAttribute("events", eventViews);
         model.addAttribute("companies", companies);
+        model.addAttribute("type", "Register");
 //        model.addAttribute("members", new MemberModel());
-        model.addAttribute("view", "member/member-add");
+        model.addAttribute("view", "members/member-add");
         return "base-layout";
     }
 
@@ -199,7 +182,7 @@ public class MemberController {
                                 @RequestParam String memberType,
                                 @RequestParam Boolean isPayed,
                                 @RequestParam String event,
-//                                @RequestParam String recommendedName,
+//                              @RequestParam String recommendedName,
                                 @RequestParam(required = false) Long id,
                                 @RequestParam String pcPlatform,
                                 @RequestParam List<String> applicationsList) {
@@ -213,12 +196,12 @@ public class MemberController {
             model.addAttribute("membersViews", memberViews);
             model.addAttribute("events", eventViews);
             model.addAttribute("companies", companies);
-            model.addAttribute("view", "/member/member-add");
+            model.addAttribute("view", "/members/member-add");
             return "base-layout";
         }
 
-        Company companyModel = this.companyService.getCompanyByName(companyName);
-        memberModel.setCompany(companyModel);
+        Company company = this.companyService.getCompanyByName(companyName);
+        memberModel.setCompany(company);
 
         if (!(event.equals(""))) {
             Event eventToPersist = this.eventService.getEventByName(event);
@@ -278,7 +261,7 @@ public class MemberController {
 
         this.memberService.persist(memberModel);
 
-        return "redirect:/member/all";
+        return "redirect:/members/all";
 
     }
 
@@ -397,7 +380,7 @@ public class MemberController {
         }
 
         model.addAttribute("members", memberViewAnnualInstallments);
-        model.addAttribute("view", "/member/member-table-annual-installment");
+        model.addAttribute("view", "/members/member-table-annual-installment");
         return "base-layout";
 
     }
